7c098daeae28469c985c324e255bf0f284408cf4
Rajkumar Jayakumar <rajkumarmca1982@outlook.com>
2022-04-07 21:02:47 +0530

    Allowed global cors

--- src/main/java/com/rajutechies/contactusapp/ContactUsAppApplication.java
+++ src/main/java/com/rajutechies/contactusapp/ContactUsAppApplication.java
@@ -2,12 +2,25 @@ package com.rajutechies.contactusapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ContactUsAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContactUsAppApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/contactservice").allowedOrigins("*");
+            }
+        };
     }
 
 }
